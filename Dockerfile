affc68cf04bcf99a59e8e87418be2a8554a0b69d
trueosiris <tim@chaubet.be>
2022-12-01 09:13:22 +0100

    first docker image push

--- Dockerfile
+++ Dockerfile
@@ -3,14 +3,22 @@ FROM ubuntu:22.04
 LABEL name="checkmk-agent" \
       maintainer="Trueosiris" 
 ENV TZ="Europe/Brussels"
-VOLUME ["plugins"]
+ENV AGENTPACKAGE="https://checkmk.timmer.ninja/download_agent.py?hash=05833099304e914e&os=linux_deb"
+ENV INSTALLED="FALSE"
+VOLUME ["/install_packages", "plugins", "/etc/xinetd.d"]
 ARG DEBIAN_FRONTEND="noninteractive"
 RUN apt update -y && \
     apt-get upgrade -y && \
     apt-get install -y  apt-utils && \
     apt-get install -y  software-properties-common \
                         tzdata \ 
-			xinetd
+			xinetd \
+			wget \
+			curl \
+			python2.7 \
+			python3 && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/
 EXPOSE 6556
 COPY start.sh /start.sh
 RUN chmod +x /start.sh
--- start.sh
+++ start.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
 echo "Executing start.sh ..."
+echo "Stopping xinetd ..."
+service xinetd stop 2>&1
+
+
+x=/etc/xinetd.d/checkmk-agent
+ap=$AGENTPACKAGE
+i=$INSTALLED
+if [ -z $x ]; then
+	echo "xinetd.d file for checkmk does not exist. creating $x."
+	echo "service check_mk" > $x
+	echo "{" >> $x
+	echo "	type           = UNLISTED" >> $x
+	echo "	port           = 6556" >> $x
+	echo "	socket_type    = stream" >> $x
+	echo "	protocol       = tcp" >> $x
+	echo "	wait           = no" >> $x
+	echo "	user           = root" >> $x
+	echo "	server         = /usr/bin/check_mk_agent" >> $x
+	echo "	#only_from      = 10.10.12.2" >> $x
+	echo "	disable        = no" >> $x
+	echo "}" >> $x
+fi
+echo "agentpackage source = $ap"
+if [ -z "/install_packages/agentpackage.deb" ]; then
+	curl $ap -o /install_packages/agentpackage.deb
+fi
+echo "installed = $i"
+if [ $i == "FALSE" ]; then
+	dpkg -i /install_packages/agentpackage.deb
+fi
+
+/usr/sbin/xinetd -filelog /var/log/xinetd2.log -stayalive
